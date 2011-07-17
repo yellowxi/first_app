411336da952d94538cba36eb57916088d8f55525
yellowxi <yellowxi@gmail.com>
2011-07-17 17:26:20 +0800

    Init the Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo1::Application.config.secret_token = '5e3c884e64926bcbb2810f2b0d5e01f81355ccbb3e3db46f67e213af1dfdf1ecc9ed27e595ee3e416c63d925e26ab936583c096ef5a0abcce9d85297684f75a7'
